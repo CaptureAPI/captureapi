66319e67af022cda7c0e99697e3535ea39e5d22a
Christian Joudrey <cmallette@gmail.com>
2012-09-05 22:50:14 -0400

    Add scheme to configuration.

--- lib/captureapi/api.rb
+++ lib/captureapi/api.rb
@@ -6,8 +6,6 @@ module CaptureAPI
   module API
     class << self
       def thumbnail_uri(url, options = {})
-        configuration = CaptureAPI::configuration
-
         options[:width]         ||= 200
         options[:height]        ||= 200
         options[:cache]         ||= '1h'
@@ -15,25 +13,26 @@ module CaptureAPI
 
         query = {
           :url           => url,
-          :key_id        => configuration.api_key_id,
+          :key_id        => CaptureAPI.configuration.api_key_id,
           :cache         => options[:cache],
           :hash          => request_hash(url),
           :manipulations => options[:manipulations]
         }
 
         uri = URI::HTTP.build({
-          :host  => configuration.host,
+          :host  => CaptureAPI.configuration.host,
           :path  => '/inline_capture',
           :query => query.to_query
         })
 
-        uri.scheme = 'https'
+        uri.scheme = CaptureAPI.configuration.scheme
         uri
       end
 
+      # The request_hash is built by computing the SHA256 of the url, api_key and current date (Y-M-D).
+      # This hash is used to authenticate the request.
       def request_hash(url)
-        configuration = CaptureAPI::configuration
-        api_key = configuration.api_key
+        api_key = CaptureAPI.configuration.api_key
         date = Time.now.utc.strftime('%Y-%m-%d')
         hash_data = "#{url}#{api_key}#{date}"
         digest = Digest::SHA2.new(256)
--- lib/captureapi/configuration.rb
+++ lib/captureapi/configuration.rb
@@ -1,7 +1,7 @@
 module CaptureAPI
   # Used to set up and modify settings for the requests.
   class Configuration
-    OPTIONS = [:api_key, :api_key_id, :host].freeze
+    OPTIONS = [:api_key, :api_key_id, :scheme, :host].freeze
 
     # The API key for your user.
     attr_accessor :api_key
@@ -9,11 +9,15 @@ module CaptureAPI
     # The API key id for your user.
     attr_accessor :api_key_id
 
+    # The scheme to use for requests (defaults to https).
+    attr_accessor :scheme
+
     # The host to connect to (defaults to api.captureapi.com).
     attr_accessor :host
 
     def initialize
       @host = 'api.captureapi.com'
+      @scheme = 'https'
     end
   end
 end
